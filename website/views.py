b1a755c3aa55ca17405a41f3999c31e729aa42e7
Senoor22s <legendsdt2@gmail.com>
2025-02-15 11:03:00 +0330

    render
    ...

--- website/views.py
+++ website/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1> Home Page </h1>')
+    return render(request,'New Folder/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1> about Page </h1>')
+    return render(request,'New Folder/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1> Contact Page </h1>')
+    return render(request,'New Folder/contact.html')
