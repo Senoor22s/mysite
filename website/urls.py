a38a5df575f8b702c866ede67dc0eb56f737072a
Senoor22s <legendsdt2@gmail.com>
2025-02-19 11:56:26 +0330

    episode 5
    fixing buttons

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,16 @@
 from website.views import *
 from django.urls import path
 
-urlpatterns = [
-    path('',index_view),
-    path('about',about_view),
-    path('contact',contact_view)
+app_name= 'website'
 
+urlpatterns = [
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+    path('blog',blog_view,name='blog'),
+    path('404',page_404_view,name='404'),
+    path('feature',feature_view,name='feature'),
+    path('product',product_view,name='product'),
+    path('team',team_view,name='team'),
+    path('testimonial',testimonial_view,name='testimonial'),
 ]
--- website/views.py
+++ website/views.py
@@ -8,3 +8,21 @@ def about_view(request):
 
 def contact_view(request):
     return render(request,'New Folder/contact.html')
+
+def blog_view(request):
+    return render(request,'blog/blog.html')
+
+def page_404_view(request):
+    return render(request,'pages/404.html')
+
+def feature_view(request):
+    return render(request,'pages/feature.html')
+
+def product_view(request):
+    return render(request,'pages/product.html')
+
+def team_view(request):
+    return render(request,'pages/team.html')
+
+def testimonial_view(request):
+    return render(request,'pages/testimonial.html')
